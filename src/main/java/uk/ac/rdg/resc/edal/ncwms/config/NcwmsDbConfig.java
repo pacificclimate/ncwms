266fa5a5c79a2af9419f91ea68a765b0f94205ff
Rod Glover <rglover@uvic.ca>
2020-07-16 17:44:00 -0700

    Use ResultSetRunGrouper in NcwmsDbConfig#loadFromIndexDatabase

--- src/main/java/uk/ac/rdg/resc/edal/ncwms/config/NcwmsDbConfig.java
+++ src/main/java/uk/ac/rdg/resc/edal/ncwms/config/NcwmsDbConfig.java
@@ -2,6 +2,9 @@ package uk.ac.rdg.resc.edal.ncwms.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.ac.rdg.resc.edal.catalogue.jaxb.CacheInfo;
+import uk.ac.rdg.resc.edal.catalogue.jaxb.DatasetConfig;
+import uk.ac.rdg.resc.edal.catalogue.jaxb.VariableConfig;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -15,14 +18,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-
-import org.json.JSONObject;
-
-import uk.ac.rdg.resc.edal.catalogue.jaxb.CacheInfo;
-import uk.ac.rdg.resc.edal.catalogue.jaxb.DatasetConfig;
-import uk.ac.rdg.resc.edal.catalogue.jaxb.VariableConfig;
-
-import static uk.ac.rdg.resc.edal.ncwms.config.DatasetConfigFactory.makeDatasetConfig;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Properties;
 
 /**
  * Deals purely with the (de)serialisation of the ncWMS config file. This
@@ -120,7 +118,7 @@ public class NcwmsDbConfig extends NcwmsConfig {
      *
      * It's not clear that this shouldn't be part of {@link #readFromFile}.
      */
-    public void loadFromIndexDatabase() {
+    public void loadFromIndexDatabase() throws SQLException {
         // Set up a dummy DatasetStorage to handle loaded datasets.
         // Christ only knows what we should actually be doing here.
         setDatasetLoadedHandler(new DummyDatasetStorage());
@@ -128,13 +126,68 @@ public class NcwmsDbConfig extends NcwmsConfig {
         // Let's start fake: Get the new "datasets" from a bogus element
         // <result> in the <indexDataset> element. We'll decode it from JSON!
         // And we'll just specify a few parameters with it.
-        JSONObject datasetParams = new JSONObject(getIndexDatabase().getResult());
-        String id = datasetParams.getString("id");
-        String location = datasetParams.getString("location");
-        VariableConfig[] variables = new VariableConfig[]{};
-        DatasetConfig dataset = makeDatasetConfig(
-                id, id, location, true, false, "", "", "", false, -1, null, null, null, variables
-        );
-        addDataset(dataset);
+//        JSONObject datasetParams = new JSONObject(getIndexDatabase().getResult());
+//        String id = datasetParams.getString("id");
+//        String location = datasetParams.getString("location");
+//        VariableConfig[] variables = new VariableConfig[]{};
+//        DatasetConfig dataset = makeDatasetConfig(
+//                id, id, location, true, false, "", "", "", false, -1, null, null, null, variables
+//        );
+//        addDataset(dataset);
+
+        // Now we're going to get seriouser.
+        //
+        // We will need to do the following things:
+        // - Establish a connection to the database.
+        // - Issue at least one query that returns a list of information
+        //   defining the "datasets" in the database.
+        // - Convert each element of the list of dataset information to a
+        //   DatasetConfig.
+        // - Add each DatasetConfig to this object.
+
+        // Establish connection to database
+        Properties connectionProperties = new Properties();
+        connectionProperties.setProperty("password", System.getenv("INDEX_DATABASE_PASSWORD"));
+        NcwmsIndexDatabase indexDatabase = getIndexDatabase();
+        try (
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:" + indexDatabase.getUrl(),
+                        connectionProperties
+                )
+        )
+        {
+            Statement statement = connection.createStatement();
+
+            // Get the results of a query that defines datasets.
+            // The SQL for this query is defined externally in the config file in the tag
+            // <datasetsQuery>. This query must return one row per dataset.
+            // Each row must have columns named
+            // - `dataset_id`
+            // - `location`
+            // The results must be sorted in ascending order of `dataset_id`.
+            ResultSet datasetRs = statement.executeQuery(indexDatabase.getDatasetsQuery());
+
+            // Get the results of a query that defines variables.
+            // The SQL for this query is defined externally in the config file in the tag
+            // <variablesQuery>. This query must return one row per variable.
+            // Each row must have columns named
+            // - `dataset_id`
+            // - `variable_id`
+            // - `variable_name`
+            // - `range_min`
+            // - `range_max`
+            // The results must be sorted in ascending order of `dataset_id`.
+            ResultSet variableRs = statement.executeQuery(indexDatabase.getVariablesQuery());
+
+            ResultSetToDatasetConfig datasetConfigMaker = new ResultSetToDatasetConfig();
+            ResultSetToVariableConfig variableConfigMaker = new ResultSetToVariableConfig();
+            ResultSetRunGrouper<DatasetConfig, VariableConfig> grouper =
+                    new ResultSetRunGrouper<>("id", datasetConfigMaker, variableConfigMaker);
+
+            ArrayList<DatasetConfig> datasets = grouper.group(datasetRs, variableRs);
+            for (DatasetConfig dataset: datasets) {
+                addDataset(dataset);
+            }
+        }
     }
 }
